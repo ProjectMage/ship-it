fee14e8cc9524920d4025f688f1109f6581b11a2
StillBeginner <StillBeginner@users.noreply.github.com>
2025-11-05 18:17:08 +0100

    Initial prototype

---
+++ game.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+var window: Window
+var gold: int = 0
+@onready var shipPath: PathFollow2D = $PathToGold/PathFollow2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	window = get_window()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	shipPath.progress_ratio += delta * 0.1
+
+
+func _on_transparent_button_toggled(toggled_on: bool) -> void:
+	window.transparent = toggled_on
+	window.transparent_bg = toggled_on
+
+func _on_border_button_toggled(toggled_on: bool) -> void:
+	window.borderless = toggled_on
+
+
+func _on_always_on_top_button_toggled(toggled_on: bool) -> void:
+#	var borderless = window.borderless
+#	window.borderless = true
+	window.always_on_top = toggled_on
+#	window.borderless = borderless
+
+func _on_no_focus_button_toggled(toggled_on: bool) -> void:
+	var borderless = window.borderless
+	window.borderless = true
+	window.unfocusable = toggled_on
+	window.borderless = borderless
+
+
+func _on_island_area_entered(_area: Area2D) -> void:
+	$Island/Label.text = str(gold) + " Gold"
+	gold += 1
+	
